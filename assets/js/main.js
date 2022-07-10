cd281299e9ccaa8db4c3ea9c264e6717925ac331
GuilleContreras <guille.contre1@gmail.com>
2022-07-10 14:46:36 -0300

    Mejora de funciones y estructura del Main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,7 @@
 let guitarras = 0;
 let bajos = 0;
 let baterias = 0;
-let continuar = 0;
-let nombreCliente = "";
+
 
 //Declaración de objeto constructor de producto
 
@@ -16,8 +15,20 @@ class producto {
     this.price = parseFloat(precio);
     this.category = categoria;
     this.stock = stock;
+    this.vendido = false;
+    }
+
+    venderProducto() {
+        this.vendido = true;
+    }
+
+    fueVendido() {
+        return this.vendido;
     }
 }
+
+//Declaración de Instancias de la clase producto
+
 const fenderStratocaster = new producto("Fender Stratocaster", 3000, "guitarra", 10);
 const gibsonlesPaul = new producto("Gibson Les Paul", 4500, "guitarra", 10);
 const fenderTelecaster = new producto("Fender Telecaster", 3500, "guitarra", 10);
@@ -28,20 +39,55 @@ const dbDrums_bateria = new producto("Batería dbDrums", 8500, "batería", 10);
 const gretsch_bateria = new producto("Batería Gretsch", 7900, "batería", 10);
 const mapex_bateria = new producto("Batería Mapex", 6900, "batería", 10);
 
+//Declaración de función Nombre
 
+function nombreCliente() {
+    let nombre;
+    nombre = document.getElementById("tagNombre").value;
+    console.log(nombre);
+    return nombre;
+}
 
-//Declaración de función saludo
+let nombre = nombreCliente();
 
-function saludo() {
-    alert("Somos Rock All!");
-    let nombre = prompt ("Para comenzar decinos tu nombre");
-  
-    while (nombre === "") {
-      nombre = prompt("Para comenzar decinos tu nombre");
+//Declaración de función Presupuesto
+
+function presupuestoCliente() {
+    let presupuesto;
+    presupuesto = document.getElementById("tagPresupuesto").value;
+    console.log(presupuesto);
+    return presupuesto;
+
+}
+
+let presupuesto = presupuestoCliente();
+
+function datosIniciales() {
+    nombreCliente();
+    presupuestoCliente();
+}
+//Declaración de función continuar
+
+function continuar (){
+
+    let continuar = prompt("¿Deseas seguir comprando?, 1- SI, 2- NO.")
+
+    if (continuar == 1){
+        seleccion = prompt ("¿Qué estás buscando?, " + "1- " + categorias[0] + ", 2- " + categorias[1] + ", 3- " + categorias[2]);
+        if (seleccion == 1){
+            guitarShop();
+        }
+        if (seleccion == 2){
+            bassShop();
+        }
+        if (seleccion == 3){
+            drumShop();
+        }
     }
-    alert("Bienvenido a Rock All " + nombre + ", tu tienda de instrumentos musicales");
-  }
-saludo();
+    else{
+        alert("Gracias por tu visita, vuelve a recargar la pagina para comenzar nuevamente.")
+    }
+}
 
   //Declaración de Arrays
 
@@ -71,21 +117,21 @@ for (const content of drum){
     console.log(content.name);
 }
 
-
-
 //Selección de articulo y compra
 
-var seleccion = prompt (" ¿Qué estás buscando?, " + "1- " + categorias[0] + ", 2- " + categorias[1] + ", 3- " + categorias[2]);
-let presupuesto = parseFloat(prompt("¿Cual es tu presupuesto para esta compra?"));
 
-while (continuar !== 2){
+ 
 
+let boton1 = document.getElementById("btnGuitars")
+boton1.addEventListener("click", guitarShop)
 
+function guitarShop (){
+     let presupuesto;
 
-if (seleccion == 1) {
     alert("Bienvenido a nuestra sección de Guitarras!");
     
-    let guitarras = prompt("¿Qué guitarra deseas comprar?, 1- Fender Stratocaster, 2- Gibson Les Paul, 3- Fender Telecaster");
+    
+    let guitarras = prompt(nombre + ", ¿Qué guitarra deseas comprar?,\n1- Fender Stratocaster\n2- Gibson Les Paul\n3- Fender Telecaster");
     if (guitarras == 1){
         if (presupuesto >= fenderStratocaster.price){
             
@@ -95,6 +141,7 @@ if (seleccion == 1) {
             presupuesto = Math.abs(fenderStratocaster.price - presupuesto); 
             alert("Felicitaciones!, has comprado una " + fenderStratocaster.category + " " + fenderStratocaster.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
@@ -110,6 +157,7 @@ if (seleccion == 1) {
             presupuesto = Math.abs(gibsonlesPaul.price - presupuesto); 
             alert("Felicitaciones!, has comprado una " + gibsonlesPaul.category + " " + gibsonlesPaul.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
@@ -126,28 +174,37 @@ if (seleccion == 1) {
             presupuesto = Math.abs(fenderTelecaster.price - presupuesto); 
             alert("Felicitaciones!, has comprado una " + fenderTelecaster.category + " " + fenderTelecaster.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
             alert("No tienes suficiente dinero para realizar la compra.");
         }
     }
-    else {
-        alert("Tu selección no es válida, por favor volvé a elegir nuevamente.");
-    }
 
-} else if (seleccion == 2){
+
+}
+
+let boton2 = document.getElementById("btnBass")
+boton2.addEventListener("click", bassShop)
+
+function bassShop (){
+    let nombre = document.getElementById("tagNombre").value;
+    let presupuesto = document.getElementById("tagPresupuesto").value;
+   
+
     alert("Bienvenido a nuestra sección de Bajos");
-    let bajos = prompt("¿Qué bajo deseas comprar?, 1- Fender Jazz Bass, 2- Gibson Les Paul, 3- Fender Presicion Bass  ");
+    let bajos = prompt(nombre + ", ¿Qué bajo deseas comprar?, 1- Fender Jazz Bass, 2- Gibson Les Paul, 3- Fender Presicion Bass  ");
     if (bajos == 1){
         if (presupuesto >= fenderJazz_bass.price){
             
-            let confirmacion = parseFloat(prompt("Estás a punto de comprar una " + fenderJazz_bass.name + " , ¿deseas confirmar la compra? 1-SI 2-NO"));
+            let confirmacion = parseFloat(prompt("Estás a punto de comprar un " + fenderJazz_bass.name +  " de" + " $" + fenderJazz_bass.price + " , ¿deseas confirmar la compra? 1-SI 2-NO"));
             
             if (confirmacion == 1){
             presupuesto = Math.abs(fenderJazz_bass.price - presupuesto); 
             alert("Felicitaciones!, has comprado un " + fenderJazz_bass.category + " " + fenderJazz_bass.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
@@ -158,12 +215,13 @@ if (seleccion == 1) {
     else if (bajos == 2){
         if (presupuesto >= gibsonLespaul_bass.price){
             
-            let confirmacion = parseFloat(prompt("Estás a punto de comprar una " + gibsonLespaul_bass.name + " , ¿deseas confirmar la compra? 1-SI 2-NO"));
+            let confirmacion = parseFloat(prompt("Estás a punto de comprar un " + gibsonLespaul_bass.name + " , ¿deseas confirmar la compra? 1-SI 2-NO"));
             
             if (confirmacion == 1){
             presupuesto = Math.abs(gibsonLespaul_bass.price - presupuesto); 
             alert("Felicitaciones!, has comprado un " + gibsonLespaul_bass.category + " " + gibsonLespaul_bass.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
@@ -174,12 +232,13 @@ if (seleccion == 1) {
     else if (bajos == 3){
         if (presupuesto >= fenderPresicion_bass.price){
             
-            let confirmacion = parseFloat(prompt("Estás a punto de comprar una " + fenderPresicion_bass.name + " , ¿deseas confirmar la compra? 1-SI 2-NO"));
+            let confirmacion = parseFloat(prompt("Estás a punto de comprar un " + fenderPresicion_bass.name + " , ¿deseas confirmar la compra? 1-SI 2-NO"));
             
             if (confirmacion == 1){
             presupuesto = Math.abs(fenderPresicion_bass.price - presupuesto); 
             alert("Felicitaciones!, has comprado un " + fenderPresicion_bass.category + " " + fenderPresicion_bass.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
@@ -190,9 +249,17 @@ if (seleccion == 1) {
         alert("Tu selección no es válida, por favor volvé a elegir nuevamente.");
     }
 
-} else if (seleccion == 3){
+} 
+
+let boton3 = document.getElementById("btnDrum")
+boton3.addEventListener("click", drumShop)
+
+function drumShop (){
+    let nombre = document.getElementById("tagNombre").value;
+    let presupuesto = document.getElementById("tagPresupuesto").value;
+    
     alert("Bienvenido a nuestra sección de Baterias");
-    let baterias = prompt("¿Qué bateria deseas comprar?, 1- Db Drums, 2- Gretsch, 3- Mapex");
+    let baterias = prompt(nombre + ", ¿Qué batería deseas comprar?, 1- Db Drums, 2- Gretsch, 3- Mapex");
     if (baterias == 1){
         if (presupuesto >= dbDrums_bateria.price){
             
@@ -202,6 +269,7 @@ if (seleccion == 1) {
             presupuesto = Math.abs(dbDrums_bateria.price - presupuesto); 
             alert("Felicitaciones!, has comprado una " + dbDrums_bateria.category + " " + dbDrums_bateria.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
@@ -217,6 +285,7 @@ if (seleccion == 1) {
             presupuesto = Math.abs(gretsch_bateria.price - presupuesto); 
             alert("Felicitaciones!, has comprado una " + gretsch_bateria.category + " " + gretsch_bateria.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
@@ -232,6 +301,7 @@ if (seleccion == 1) {
             presupuesto = Math.abs(mapex_bateria.price - presupuesto); 
             alert("Felicitaciones!, has comprado una " + mapex_bateria.category + " " + mapex_bateria.name);
             alert("Te quedan $" + presupuesto + " para continuar comprando." );
+            continuar();
         }
         
         } else {
@@ -242,24 +312,4 @@ if (seleccion == 1) {
         alert("Tu selección no es válida, por favor volvé a elegir nuevamente.");
     }
 
-} else {
-    alert("Tu selección no es válida, por favor volvé a elegir nuevamente");
 }
-
-let continuar = prompt("¿Deseas seguir comprando?, 1- SI, 2- NO.")
-
-    if (continuar == 1){
-        seleccion = prompt (" ¿Qué estás buscando?, " + "1- " + categorias[0] + ", 2- " + categorias[1] + ", 3- " + categorias[2]);
-        
-    }
-    else{
-        break;
-    }
-    
-
-}
-
-
-alert("Gracias por tu visita, vuelve a recargar la pagina para comenzar nuevamente.")
-
-
